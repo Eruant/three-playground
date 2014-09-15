b753f14f58d72d898d91add448d10166d59d1330
Matt Gale <matt@littleball.co.uk>
2014-09-15 13:55:25 +0100

    adding more meshes

--- dest/www/js/bundle.js
+++ dest/www/js/bundle.js
@@ -4,12 +4,15 @@
   var THREE = require('./lib/three.min');
   THREE = THREE || window.THREE;
 
-  var scene, camera, renderer, geometry, material, cube;
+  var scene, camera, renderer, geometry, material, cube, light, cube2;
 
   var render = function () {
 
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    
+    cube2.rotation.x -= 0.01;
+    cube2.rotation.z += 0.01;
 
     renderer.render(scene, camera);
     window.requestAnimationFrame(render);
@@ -33,8 +36,21 @@
 
     cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
-    camera.position.z = 5;
 
+    material = new THREE.MeshBasicMaterial({
+      color: 0x0000ff
+    });
+    cube2 = new THREE.Mesh(geometry, material);
+    cube2.position.y -= 2;
+    scene.add(cube2);
+
+    light = new THREE.PointLight(0xffffff);
+    light.position.x = 0;
+    light.position.y = 100;
+    light.position.z = 0;
+    scene.add(light);
+
+    camera.position.z = 5;
     render();
   };
 
